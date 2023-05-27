d43f23204b95f64423015f25803c262f4a01821d
jmsmith23 <smitjeff23@gmail.com>
2023-05-27 12:19:11 -0500

    Write code for API hw assignment. Not yet fully completed

--- server.js
+++ server.js
@@ -7,3 +7,41 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`I'm all ears on port ${port}`);
 });
+
+let items = [];
+
+//Return a list of items
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+//Accept JSON body with name and price fields, add a new item to the list, and return the added item with a generated id
+app.post("/items", (req, res) => {
+  items.push(req.body);
+  res.status(201).json(req.body);
+});
+
+//Return the item with the given id
+app.get("/items/:id", (req, res) => {
+  const item = items.find((b) => b.id === parseInt(req.params.id));
+  if (!item) return res.status(404).json({ message: "Item not found" });
+  res.json(item);
+});
+
+//Accept a JSON body with name and price fields, update the item with the given id, and return the updated item
+app.put("/items/:id", (req, res) => {
+  const item = items.find((b) => b.id === parseInt(req.params.id));
+  if (!item) return res.status(404).json({ message: "Item not found" });
+
+  Object.assign(item, req.body);
+  res.json(item);
+});
+
+//Delete the item with the given id and return the deleted item
+app.delete("/items/:id", (req, res) => {
+  const index = items.findIndex((b) => b.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).json({ message: "Item not found" });
+
+  items.splice(index, 1);
+  res.sendStatus(204);
+});
